182b5eeab8ee435d693b407a0c57a0c484198aef
Hiromu Miyazaki <miyazaki@arch.cs.titech.ac.jp>
2020-11-06 13:09:16 +0900

    add printing  array.shape

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -18,6 +18,7 @@ def init_network():
     return network
 
 
+cnt = 0
 def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
@@ -28,6 +29,31 @@ def predict(network, x):
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
     y = softmax(a3)
+
+    global cnt
+    if (cnt==0):
+        print("a1 = np.dot(x, W1) + b1")
+        print("z1 = sigmoid(a1)")
+        print("     x.shape : "+str(x.shape))
+        print("    W1.shape : "+str(W1.shape))
+        print("    b1.shape : "+str(b1.shape))
+        print("    a1.shape : "+str(a1.shape))
+        print("    z1.shape : "+str(z1.shape))
+        print("a2 = np.dot(z1, W2) + b2")
+        print("z2 = sigmoid(a2)")
+        print("    z1.shape : "+str(z1.shape))
+        print("    W2.shape : "+str(W2.shape))
+        print("    b2.shape : "+str(b2.shape))
+        print("    a2.shape : "+str(a2.shape))
+        print("    z2.shape : "+str(z2.shape))
+        print("a3 = np.dot(z2, W3) + b3")
+        print(" y = softmax(a3)")
+        print("    z2.shape : "+str(z2.shape))
+        print("    W3.shape : "+str(W3.shape))
+        print("    b3.shape : "+str(b3.shape))
+        print("    a3.shape : "+str(a3.shape))
+        print("     y.shape : "+str(y.shape))
+    cnt = cnt + 1
 
     return y
 
