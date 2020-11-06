f40956ea3d289400b7471171a4ba246badc22348
Hiromu Miyazaki <miyazaki@arch.cs.titech.ac.jp>
2020-11-06 15:49:01 +0900

    Update gradient_method.py

--- ch04/gradient_method.py
+++ ch04/gradient_method.py
@@ -22,9 +22,13 @@ def function_2(x):
 
 init_x = np.array([-3.0, 4.0])    
 
-lr = 0.1
+#lr = 0.1
+print("input lr : ", end="")
+lr = float(input())
 step_num = 20
 x, x_history = gradient_descent(function_2, init_x, lr=lr, step_num=step_num)
+
+print(x)
 
 plt.plot( [-5, 5], [0,0], '--b')
 plt.plot( [0,0], [-5, 5], '--b')
